c1ac73178883b3a801fb1514421412d1a14fd3fb
wolfi584 <wolfi584@hotmail.com>
2021-07-10 21:51:21 -0500

    Lession 12 Modules

---
+++ data-12-2-1-resources/plots.js
@@ -0,0 +1,5 @@
+var numbers = [1,2,3,4,5];
+var doubled = numbers.map(function(num){
+    return num * 2;
+});
+console.log(doubled);
---
+++ data-12-3-2-resources/spacex.js
@@ -0,0 +1,1 @@
+d3.json("samples.json").then(receivedData => console.log(receivedData));
